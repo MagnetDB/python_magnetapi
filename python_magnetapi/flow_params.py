7f615081477a9f16c0920ddf40021a10269e3802
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2023-01-04 14:41:25 +0100

    Fix flow params estimation

--- python_magnetapi/flow_params.py
+++ python_magnetapi/flow_params.py
@@ -45,76 +45,78 @@ def compute(api_server: str, headers: dict, oid: int, mtype: str='magnet', debug
         'M10': { 'Rpm': 'Rpm2', 'Flow': 'Flow2', 'rlist' : []},
     }
 
-    # MAYBE better to proceed first by site
-    # instead of getting all records at once ??
-    records = utils.gethistory(api_server, headers, oid, mtype, debug)
+    sites = utils.gethistory(api_server, headers, oid, mtype=mtype, otype='site', debug=debug)
     if debug:
-        print(f'records: {records}')
+        print(f'sites: {sites}')
 
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
         print(f'moving to {tempdir}')
 
-        for key in fit_data:
-            fit_data[key]['rlist'] = [record for record in records if key in record['name']]
-
+        for site in sites:
+            # print(f"site: {site}, id={site['site_id']}")
+            records = utils.gethistory(api_server, headers, site['site_id'], mtype='site', otype='record', verbose=debug, debug=debug)
+            # print(f'records: {records}')
+            
             # download files
             files = []
             total = 0
-            nrecors = len(fit_data[key]['rlist'])
-            for i in track(range(nrecors), description=f"Processing records for {key}..."):
-                f = fit_data[key]['rlist'][i]
+            nrecords = len(records)
+            housing = None
+            for i in track(range(nrecords), description=f"Processing records for site {site['site']['name']}..."):
+                f = records[i]
                 # print(f'f={f}')
-                # id = f['id']
-                # data = utils.getobject(api_server, headers, id=id, mtype='record', debug=debug)
-
                 attach = f['attachment_id']
-                files.append(utils.download(api_server, headers, attach, debug))
+                filename = utils.download(api_server, headers, attach, verbose=debug, debug=debug)
+                housing = filename.split('_')[0]
+                files.append(filename)
                 total += 1
+                if i >= 10: break
             print(f"Processed {total} records.")
 
-            # get keys to be extracted
-            df = pd.read_csv(files[0], sep='\s+', engine='python', skiprows=1)
-            Ikey = "tttt"
+            if files:
+                # get keys to be extracted
+                df = pd.read_csv(files[0], sep='\s+', engine='python', skiprows=1)
+                Ikey = "tttt"
 
-            # get first Icoil column (not necessary Icoil1)
-            keys = df.columns.values.tolist()
+                # get first Icoil column (not necessary Icoil1)
+                keys = df.columns.values.tolist()
 
-            # key firs header that match Icoil\d+
-            for _key in keys:
-                _found = re.match("Icoil\d+", _key)
-                if _found:
-                    Ikey = _found.group()
-                    print(f"Ikey={Ikey}")
-                    break
+                # key firs header that match Icoil\d+
+                for _key in keys:
+                    _found = re.match("Icoil\d+", _key)
+                    if _found:
+                        Ikey = _found.group()
+                        print(f"Ikey={Ikey}")
+                        break
 
-            plot_files(files, key1=Ikey ,key2=fit_data[key]['Rpm'], show=debug, debug=debug)
-            df = concat_files(files, keys=[Ikey, fit_data[key]['Rpm']], show=debug, debug=debug)
-            pairs = [f"{Ikey}-{fit_data[key]['Rpm']}"]
+                plot_files(files, key1=Ikey ,key2=fit_data[housing]['Rpm'], show=debug, debug=debug)
+                df = concat_files(files, keys=[Ikey, fit_data[housing]['Rpm']], debug=debug)
+                pairs = [f"{Ikey}-{fit_data[housing]['Rpm']}"]
 
-            def vpump_func(x, a: float, b: float):
-                return a * (x/Imax)**2 + b
+                def vpump_func(x, a: float, b: float):
+                    return a * (x/Imax)**2 + b
 
-            df.replace([np.inf, -np.inf], np.nan, inplace=True)
-            df.dropna(inplace=True)
+                df.replace([np.inf, -np.inf], np.nan, inplace=True)
+                df.dropna(inplace=True)
 
-            # # drop values for Icoil1 > Imax
-            result = df.query(f'{Ikey} <= {Imax}') #, inplace=True)
-            if result is not None:
-                print(f'df: nrows={df.shape[0]}, results: nrows={result.shape[0]}')
+                # # drop values for Icoil1 > Imax
+                result = df.query(f'{Ikey} <= {Imax}') #, inplace=True)
+                if result is not None:
+                    print(f'df: nrows={df.shape[0]}, results: nrows={result.shape[0]}')
 
-            x_data = result[f'{Ikey}'].to_numpy()
-            y_data = result[fit_data[key]['Rpm']].to_numpy()
-            params, params_covariance = optimize.curve_fit(vpump_func, x_data, y_data) #, p0=[flow_params['Vp0']['value'], flow_params['Vpmax']['value'] = params[1]])
-            print(f'result params: {params}')
-            print(f'result covariance: {params_covariance}')
-            print(f'result stderr: {np.sqrt(np.diag(params_covariance))}')
-            flow_params['Vp0']['value'] = params[0]
-            flow_params['Vpmax']['value'] = params[1]
+                x_data = result[f'{Ikey}'].to_numpy()
+                y_data = result[fit_data[housing]['Rpm']].to_numpy()
+                params, params_covariance = optimize.curve_fit(vpump_func, x_data, y_data)
+                print(f'result params: {params}')
+                print(f'result covariance: {params_covariance}')
+                print(f'result stderr: {np.sqrt(np.diag(params_covariance))}')
+                flow_params['Vp0']['value'] = params[0]
+                flow_params['Vpmax']['value'] = params[1]
 
-            # save flow_params
-            filename = f"{cwd}/flow_params.json"
-            with open(filename, 'w') as f:
-                f.write(json.dumps(flow_params, indent=4))
+                # save flow_params
+                filename = f"{cwd}/{site['site']['name']}-flow_params.json"
+                with open(filename, 'w') as f:
+                    f.write(json.dumps(flow_params, indent=4))
 
         os.chdir(cwd)
