4a5311f32f61a124a5b9b6dd2d69fbc0e797c499
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2023-03-30 16:35:35 +0200

    fix water flow params calc use with either Bitter or PolyHelices insert

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 ## Pre-requisites
 
-Add the following machines in /etc/hosts with IP of magnetdb
+Add the following machines in /etc/hosts with IP of MagnetDB
 
 # MagnetDB
 
 ```
-147.173.xx.yy handler.sso.grenoble.lncmi.local api.manager.sso.grenoble.lncmi.local manager.sso.grenoble.lncmi.local sso.grenoble.lncmi.local test.sso.grenoble.lncmi
-147.173.xx.yy magnetdb.grenoble.lncmi.local
-147.173.xx.yy magnetdb-api.grenoble.lncmi.local
-147.173.xx.yy magnetdb-worker.grenoble.lncmi.local
-147.173.xx.yy redis.grenoble.lncmi.local
-147.173.xx.yy postgres.grenoble.lncmi.local
-147.173.xx.yy pgadmin.grenoble.lncmi.local
-147.173.xx.yy minio.grenoble.lncmi.local
-147.173.xx.yy minio-storage.grenoble.lncmi.local
+147.173.xx.yy handler.sso.lncmig.local api.manager.sso.lncmig.local manager.sso.lncmig.local sso.lncmig.local test.sso.grenoble.lncmi
+147.173.xx.yy magnetdb.lncmig.local
+147.173.xx.yy magnetdb-api.lncmig.local
+147.173.xx.yy magnetdb-worker.lncmig.local
+147.173.xx.yy redis.lncmig.local
+147.173.xx.yy postgres.lncmig.local
+147.173.xx.yy pgadmin.lncmig.local
+147.173.xx.yy minio.lncmig.local
+147.173.xx.yy minio-storage.lncmig.local
 ```
 
 ## Running the examples
@@ -25,7 +25,7 @@ You can find your API key in your profile page.
 ```bash
 export MAGNETDB_API_KEY=xxx
 python -m python_magnetapi.cli --help
-python -m python_magnetapi.cli --name M19061901 compute --flow_params
+python -m python_magnetapi.cli --mtype magnet --name M19061901 compute --flow_params
 python -m python_magnetapi.cli --mtype part --name H15101601 compute --hoop_stress
 ```
 
--- python_magnetapi/flow_params.py
+++ python_magnetapi/flow_params.py
@@ -19,104 +19,180 @@ from . import utils
 from python_magnetrun.utils.files import concat_files
 from python_magnetrun.utils.plots import plot_files
 
-def compute(api_server: str, headers: dict, oid: int, mtype: str='magnet', debug: bool=False):
+
+def compute(api_server: str, headers: dict, oid: int, debug: bool = False):
     """
-    compute flow_params for a given objet (magnet/site)
+    compute flow_params for a given magnet
     """
-    print(f'flow_params.compute: api_server={api_server}, mtype={mtype}, id={oid}')
+    print(f"flow_params.compute: api_server={api_server}, id={oid}")
     cwd = os.getcwd()
-    print(f'cwd={cwd}')
+    print(f"cwd={cwd}")
 
     # default value
     flow_params = {
-        "Vp0": { "value" : 1000, "unit": "rpm"},
-        "Vpmax": { "value" : 2840, "unit": "rpm"},
-        "F0": { "value" : 0, "unit": "l/s"},
-        "Fmax": { "value" : 61.71612272405876 , "unit": "l/s"},
-        "Pmax": { "value" : 22, "unit": "bar"},
-        "Pmin": { "value" : 4, "unit": "bar"},
-        "Imax": { "value" : 28000, "unit": "A"}
+        "Vp0": {"value": 1000, "unit": "rpm"},
+        "Vpmax": {"value": 2840, "unit": "rpm"},
+        "F0": {"value": 0, "unit": "l/s"},
+        "Fmax": {"value": 61.71612272405876, "unit": "l/s"},
+        "Pmax": {"value": 22, "unit": "bar"},
+        "Pmin": {"value": 4, "unit": "bar"},
+        "Imax": {"value": 28000, "unit": "A"},
     }
 
-    Imax = flow_params['Imax']['value'] # 28000
+    Imax = flow_params["Imax"]["value"]  # 28000
 
+    # get magnet type: aka bitter|helix|supra ??
+    odata = utils.getobject(
+        api_server,
+        headers=headers,
+        mtype="magnet",
+        id=oid,
+        debug=debug,
+    )
+    print(f"magnet data: {json.dumps(odata, indent=2, default=str)}")
+    mname = odata["name"]
+    mpart = odata["magnet_parts"][0]
+    otype = mpart["part"]["type"]
+    # print(f"magnet type: {otype}")
+
+    # TODO: change according to magnet type
+    # or better store data with RpmH and RpmB
+    # similarely keep only Ih, Ib instead of Icoil
+    # and Ih_ref, Ib_ref instead of of Iddcct1
+    # Iddct are values of measured current
+    # Icoil  are actually referenced values required by the user
     fit_data = {
-        'M9': { 'Rpm': 'Rpm1', 'Flow': 'Flow1', 'rlist' : []},
-        'M10': { 'Rpm': 'Rpm2', 'Flow': 'Flow2', 'rlist' : []},
+        "M9": {"Rpm": "Rpm1", "Flow": "Flow1", "rlist": []},
+        "M10": {"Rpm": "Rpm2", "Flow": "Flow2", "rlist": []},
     }
+    if otype == "bitter":
+        fit_data = {
+            "M9": {"Rpm": "Rpm2", "Flow": "Flow2", "rlist": []},
+            "M10": {"Rpm": "Rpm1", "Flow": "Flow1", "rlist": []},
+        }
 
-    sites = utils.gethistory(api_server, headers, oid, mtype=mtype, otype='site', debug=debug)
+    sites = utils.gethistory(
+        api_server, headers, oid, mtype="magnet", otype="site", debug=debug
+    )
+    for i, site in enumerate(sites):
+        print(f"site[{i}/{len(sites)}]: {json.dumps(site, indent=2, default=str)}")
     if debug:
-        print(f'sites: {sites}')
+        print(f"sites: {json.dumps(sites, indent=2, default=str)}")
 
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
-        print(f'moving to {tempdir}')
+        if debug:
+            print(f"moving to {tempdir}")
 
         for site in sites:
-            # print(f"site: {site}, id={site['site_id']}")
-            records = utils.gethistory(api_server, headers, site['site_id'], mtype='site', otype='record', verbose=debug, debug=debug)
-            # print(f'records: {records}')
-            
+            sname = site["site"]["name"]
+            records = utils.gethistory(
+                api_server,
+                headers,
+                site["site_id"],
+                mtype="site",
+                otype="record",
+                verbose=debug,
+                debug=debug,
+            )
+
             # download files
             files = []
             total = 0
             nrecords = len(records)
+            print(f"site[{site['site']['name']}]: nrecords={nrecords}")
+
             housing = None
-            for i in track(range(nrecords), description=f"Processing records for site {site['site']['name']}..."):
+            for i in track(
+                range(nrecords),
+                description=f"Processing records for site {site['site']['name']}...",
+            ):
                 f = records[i]
                 # print(f'f={f}')
-                attach = f['attachment_id']
-                filename = utils.download(api_server, headers, attach, verbose=debug, debug=debug)
-                housing = filename.split('_')[0]
+                attach = f["attachment_id"]
+                filename = utils.download(
+                    api_server, headers, attach, verbose=debug, debug=debug
+                )
+                housing = filename.split("_")[0]
                 files.append(filename)
                 total += 1
-                if i >= 10: break
+                if i >= 40:
+                    break
             print(f"Processed {total} records.")
 
             if files:
                 # get keys to be extracted
-                df = pd.read_csv(files[0], sep='\s+', engine='python', skiprows=1)
+                df = pd.read_csv(files[0], sep="\s+", engine="python", skiprows=1)
                 Ikey = "tttt"
 
                 # get first Icoil column (not necessary Icoil1)
                 keys = df.columns.values.tolist()
+                print(f"{files[0]}: keys={keys}")
 
-                # key firs header that match Icoil\d+
+                # key first or latest header that match Icoil\d+ depending on mtype
+                Ikeys = []
                 for _key in keys:
-                    _found = re.match("Icoil\d+", _key)
+                    _found = re.match("(Icoil\d+)", _key)
                     if _found:
-                        Ikey = _found.group()
-                        print(f"Ikey={Ikey}")
-                        break
+                        Ikeys.append(_found.group())
+                Ikey = Ikeys[0]
+                if otype == "bitter":
+                    Ikey = Ikeys[-1]
+                print(f"Ikey={Ikey}")
 
-                plot_files(files, key1=Ikey ,key2=fit_data[housing]['Rpm'], show=debug, debug=debug)
-                df = concat_files(files, keys=[Ikey, fit_data[housing]['Rpm']], debug=debug)
+                dropped_files = []
+                for file in files:
+                    df = pd.read_csv(file, sep="\s+", engine="python", skiprows=1)
+                    if not Ikey in df.columns.values.tolist():
+                        print(f"{Ikey}: no such key in {file} - ignore {file}")
+                        dropped_files.append(file)
+
+                for file in dropped_files:
+                    files.drop(file)
+
+                # TODO update interface with name=f'{sname}_{mname}'
+                plot_files(
+                    files,
+                    key1=Ikey,
+                    key2=fit_data[housing]["Rpm"],
+                    show=debug,
+                    debug=debug,
+                    wd=cwd,
+                )
+                print(f"plot_files: key1={Ikey}, key2={fit_data[housing]['Rpm']}")
+
+                df = concat_files(
+                    files, keys=[Ikey, fit_data[housing]["Rpm"]], debug=debug
+                )
                 pairs = [f"{Ikey}-{fit_data[housing]['Rpm']}"]
+                print(f"concat_files: files={files}")
+                print(f"concat_files: keys={df.columns.values.tolist()}")
 
                 def vpump_func(x, a: float, b: float):
-                    return a * (x/Imax)**2 + b
+                    return a * (x / Imax) ** 2 + b
 
                 df.replace([np.inf, -np.inf], np.nan, inplace=True)
                 df.dropna(inplace=True)
 
                 # # drop values for Icoil1 > Imax
-                result = df.query(f'{Ikey} <= {Imax}') #, inplace=True)
+                result = df.query(f"{Ikey} <= {Imax}")  # , inplace=True)
                 if result is not None:
-                    print(f'df: nrows={df.shape[0]}, results: nrows={result.shape[0]}')
+                    print(f"df: nrows={df.shape[0]}, results: nrows={result.shape[0]}")
 
-                x_data = result[f'{Ikey}'].to_numpy()
-                y_data = result[fit_data[housing]['Rpm']].to_numpy()
-                params, params_covariance = optimize.curve_fit(vpump_func, x_data, y_data)
-                print(f'result params: {params}')
-                print(f'result covariance: {params_covariance}')
-                print(f'result stderr: {np.sqrt(np.diag(params_covariance))}')
-                flow_params['Vp0']['value'] = params[0]
-                flow_params['Vpmax']['value'] = params[1]
+                x_data = result[f"{Ikey}"].to_numpy()
+                y_data = result[fit_data[housing]["Rpm"]].to_numpy()
+                params, params_covariance = optimize.curve_fit(
+                    vpump_func, x_data, y_data
+                )
+                print(f"result params: {params}")
+                print(f"result covariance: {params_covariance}")
+                print(f"result stderr: {np.sqrt(np.diag(params_covariance))}")
+                flow_params["Vp0"]["value"] = params[0]
+                flow_params["Vpmax"]["value"] = params[1]
 
                 # save flow_params
-                filename = f"{cwd}/{site['site']['name']}-flow_params.json"
-                with open(filename, 'w') as f:
+                filename = f"{cwd}/{sname}_{mname}-flow_params.json"
+                with open(filename, "w") as f:
                     f.write(json.dumps(flow_params, indent=4))
 
         os.chdir(cwd)
